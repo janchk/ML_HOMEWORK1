4d371b47c618cbb4da6dfda50295003a37b02b48
ielay <ielay@yandex.ru>
2019-09-22 23:38:45 +0300

    Fixed MSE

--- src/stat_funcs.py
+++ src/stat_funcs.py
@@ -17,7 +17,6 @@ def MSE(ground_truth, list_of_vectors):
         MSE_res += (ground_truth - vect) ** 2
 
     MSE_res = MSE_res / len(list_of_vectors)
-    MSE_res = math.sqrt(MSE_res)
 
     return MSE_res
 #
